d6e85aff59226bd5d3ef5fba9ab4386b44f38a4e
l-wagner <lisa@ladybytes.io>
2020-08-10 00:02:10 -0400

    Fix  user rating

--- goodreads_parser.py
+++ goodreads_parser.py
@@ -12,6 +12,7 @@ with open('config.json') as f:
 API_KEY = config['GOODREADS_API_KEY']
 USER = config['GOODREADS_USER_ID']
 
+
 class book:
     def __init__(self, title, author, year, imageUrl, link, averageRating, rating=''):
         self.title = title
@@ -37,14 +38,14 @@ class book:
 
 
 r = requests.get(
-        f'https://www.goodreads.com/review/list/{USER}.xml?key={API_KEY}&v=2&shelf=read&per_page=200&page=1' )
+    f'https://www.goodreads.com/review/list/{USER}.xml?key={API_KEY}&v=2&shelf=read&per_page=200&page=1')
 
 if 'Page not found' in r.text:
-    print(f'Goodreads returned an error. Are you sure "{USER}" is a valid user id?')    
-    sys.exit() 
+    print(f'Goodreads returned an error. Are you sure "{USER}" is a valid user id?')
+    sys.exit()
 if 'Invalid API key' in r.text:
-    print('It looks like your API Key is invalid.')    
-    sys.exit() 
+    print('It looks like your API Key is invalid.')
+    sys.exit()
 
 result = xmltodict.parse(r.text)
 totalRead = int(result['GoodreadsResponse']['reviews']['@total'])
@@ -57,7 +58,7 @@ print(f'Retrieving {finalPage} {inflectedPage} from Goodreads...')
 results = []
 for i in range(1, finalPage):
     r = requests.get(
-       f'https://www.goodreads.com/review/list/{USER}.xml?key={API_KEY}&v=2&shelf=read&per_page=200&page={i}' )
+        f'https://www.goodreads.com/review/list/{USER}.xml?key={API_KEY}&v=2&shelf=read&per_page=200&page={i}')
     results.append(xmltodict.parse(r.text))
     # goodreads allows only 1 request per second
     time.sleep(2)
@@ -74,8 +75,8 @@ for result in results:
             link = review['book']['link']
             averageRating = review['book']['average_rating']
             rating = ''
-            if 'rating' in review['book']:
-                rating = review['book']['rating']
+            if 'rating' in review:
+                rating = review['rating']
 
             books.append(book(title, author, year, imageUrl,
                               link, averageRating, rating))
@@ -85,4 +86,3 @@ filename = f'./books-{today}.json'
 with open(filename, 'w') as outfile:
     json.dump([ob.__dict__ for ob in books], outfile)
 print(f'Done! {len(books)} books added to {filename}')
-
